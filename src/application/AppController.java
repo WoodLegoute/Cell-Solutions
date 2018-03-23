40fb612e193f37f6ce59b4f2f8fc8da5f1ce02ec
Wood <legoute21@gmail.com>
2018-03-22 23:57:22 -0400

    Finished FXML Gui version 1

--- src/application/AppController.java
+++ src/application/AppController.java
@@ -1,11 +1,62 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 public class AppController {
 
     @FXML
-    private CheckBox chck;
-    //testing github
+    private CheckBox optTwo;
+
+    @FXML
+    private Button buyBtn;
+
+    @FXML
+    private RadioButton packTwo;
+
+    @FXML
+    private Label closeBtn;
+
+    @FXML
+    private RadioButton packThree;
+
+    @FXML
+    private RadioButton phoneThree;
+
+    @FXML
+    private RadioButton packOne;
+
+    @FXML
+    private RadioButton phoneOne;
+
+    @FXML
+    private CheckBox Opt1;
+
+    @FXML
+    private RadioButton phoneTwo;
+
+    @FXML
+    private Label priceTxt;
+
+    @FXML
+    void voiceMailChecked(ActionEvent event) {
+
+    }
+
+
+    @FXML
+    void textMsgChecked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void buyPackage(ActionEvent event) {
+
+    }
+
+
 }
